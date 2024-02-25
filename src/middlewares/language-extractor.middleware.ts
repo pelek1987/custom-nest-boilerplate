b8ac1b34038d8ccc5a5c993df24d38087f7d30f0
Pawel Sioda <pawel.sioda@gmail.com>
2024-02-25 10:54:42 +0100

    add acceptlanguage lib to language extractor url

--- src/middlewares/language-extractor.middleware.ts
+++ src/middlewares/language-extractor.middleware.ts
@@ -1,10 +1,14 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response } from 'express';
+import acceptLanguage from 'accept-language';
+
+acceptLanguage.languages(['en-EN', 'pl-PL']);
 
 @Injectable()
 export class LanguageExtractorMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: () => void) {
-    req['language'] = req.headers['accept-language'] || 'en';
+    const acceptLanguageHeader = req.headers['accept-language'] || '';
+    req['language'] = acceptLanguage.get(acceptLanguageHeader);
     next();
   }
 }
